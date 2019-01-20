afcb7041c7357b2d9ea2f7ec472ec5ddb4e6ed4c
crozwise <31480040+crozwise@users.noreply.github.com>
2019-01-21 00:01:58 +0300

    asd
    asd

--- src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -5,6 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+//@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty" ,
+                            "json:target/cucumber.json"}) 
 public class RunCucumberTest {
 }
