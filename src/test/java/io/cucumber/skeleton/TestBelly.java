a1963ae5834c458c56f4fb05faa01ca3486ecf17
crozwise <31480040+crozwise@users.noreply.github.com>
2019-01-20 22:06:22 +0300

    asd
    asd

--- src/test/java/io/cucumber/skeleton/TestBelly.java
+++ src/test/java/io/cucumber/skeleton/TestBelly.java
@@ -1,9 +1,7 @@
 import io.cucumber.skeleton.*;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import junitparams.JUnitParamsRunner;
 
-@RunWith(JUnitParamsRunner.class)
 public class TestBelly {
    Belly belly = new Belly();
    
@@ -11,7 +9,7 @@ public class TestBelly {
    //test to check appraisal
    @Test
    public void eat(int cukes) {
-      belly.eat(12);
+      
       		
     //  assertEquals(500, appraisal, 0.0);
    }
@@ -19,7 +17,7 @@ public class TestBelly {
    // test to check yearly salary
    @Test
    public void wait(int cukes) {
-      belly.wait(12);
+      
     //  assertEquals(96000, salary, 0.0);
    }
 }
