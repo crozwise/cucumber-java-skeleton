b6fab30e3f72af620658cf9105b0258bebef1c53
crozwise <31480040+crozwise@users.noreply.github.com>
2019-01-20 22:08:07 +0300

    asd
    asd

--- src/test/java/io/cucumber/skeleton/TestBelly.java
+++ src/test/java/io/cucumber/skeleton/TestBelly.java
@@ -7,16 +7,16 @@ public class TestBelly {
    
 
    //test to check appraisal
-   @Test
-   public void eat(int cukes) {
+//   @Test
+//   public void eat(int cukes) {
       
       		
     //  assertEquals(500, appraisal, 0.0);
-   }
+//   }
 
    // test to check yearly salary
    @Test
-   public void wait(int cukes) {
+   public void growl() {
       
     //  assertEquals(96000, salary, 0.0);
    }
